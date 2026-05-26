4147f2ef71cd41d5484a1eb07835efa82c0bed91
Devanoyudhistira <devanotira@gmail.com>
2026-05-26 22:18:27 +0700

    fix:change the color of card in attendance page

--- app/student/attendance/page.jsx
+++ app/student/attendance/page.jsx
@@ -14,19 +14,19 @@ export default function Page() {
                     <h1 className="text-3xl font-bold capitalize text-white" >Kehadiran</h1>
                 </div>
                 <div className="w-full self-end justify-self-end px-4 flex justify-evenly items-center  gap-3" >
-                    <div className="flex w-25  h-20 p-4 rounded-3xl border border-gray-200 items-center justify-center gap-14" >
+                    <div className="flex w-25 h-20 p-4 rounded-3xl bg-gray-100/10 items-center justify-center gap-14" >
                         <div className="flex flex-col items-center justify-center" >
                             <h2 className="text-2xl font-bold text-white" > 12 </h2>
                             <h4 className="text-xl text-red-600 font-medium" > alfa </h4>
                         </div>
                     </div>
-                    <div className="flex w-25 h-20 p-4 rounded-3xl border border-gray-200 items-center justify-center gap-14" >
+                    <div className="flex w-25 h-20 p-4 rounded-3xl bg-gray-100/10 items-center justify-center gap-14" >
                         <div className="flex flex-col items-center justify-center" >
                             <h2 className="text-2xl font-bold text-white" > 12 </h2>
                             <h4 className="text-xl text-sky-300 font-medium" > hadir </h4>
                         </div>
                     </div>
-                    <div className="flex w-25 h-20 p-4 rounded-3xl border border-gray-200 items-center justify-center gap-14" >
+                    <div className="flex w-25 h-20 p-4 rounded-3xl bg-gray-100/10 items-center justify-center gap-14" >
                         <div className="flex flex-col items-center justify-center" >
                             <h2 className="text-2xl text-white font-bold" > 12 </h2>
                             <h4 className="text-xl text-green-400 font-medium" > sakit </h4>
@@ -41,7 +41,7 @@ export default function Page() {
                     <h1 className="text-2xl font-medium" > desember 2026 </h1>
                 </div>
                 <ul className="flex flex-col py-3 gap-4" >
-                    <li className="w-[90%] ml-4 flex flex-col px-3 py-2 rounded-3xl h-max bg-gray-200" >
+                    <li className="w-[90%] ml-4 flex flex-col px-3 py-2 rounded-3xl h-max bg-gray-100" >
                         <div className="flex w-full justify-between items-center" >
                             <h4 className="text-xl px-4 py-2 w-max h-max text-green-800 bg-emerald-200 rounded-full font-semibold" > Hadir </h4>
                             <h2 className="text-gray-600 font-medium text-xl" > senin agustus 25 </h2>
@@ -53,7 +53,7 @@ export default function Page() {
                             <h2 className="text-gray-600 font-medium text-xl" > kamis agustus 25 </h2>
                         </div>                    
                     </li>
-                    <li className="w-[90%] ml-4 flex flex-col px-3 py-2 rounded-3xl h-max bg-gray-200" >
+                    <li className="w-[90%] ml-4 flex flex-col px-3 py-2 rounded-3xl h-max bg-gray-100" >
                         <div className="flex w-full justify-between items-center" >
                             <h4 className="text-xl px-4 py-2 w-max h-max text-yellow-800 bg-yellow-200 rounded-full font-semibold" > telat </h4>
                             <h2 className="text-gray-600 font-medium text-xl" > jum'at agustus 25 </h2>
