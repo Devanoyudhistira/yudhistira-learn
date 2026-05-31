f2403b56220a0029445927657f063ad0ed9a5253
Devanoyudhistira <devanotira@gmail.com>
2026-05-31 10:03:17 +0700

    fix:now profile page will reflect to real data from database

--- app/components/teacher/navbar.jsx
+++ app/components/teacher/navbar.jsx
@@ -4,14 +4,14 @@ import { createClient } from "@/app/supabase/server";
 export default async function Navbar(){   
     const supabaseauth = await createClient()
     const { data: id } = await supabaseauth.auth.getUser()    
-    const { data: user, error: usererror } = await supabase.schema("sekolah").from("students").select("name").eq("email", id.user.id).single()     
+    const { data: user, error: usererror } = await supabase.schema("sekolah").from("teacher").select("name").eq("email", id.user.id).single()         
     return (
         <nav className="w-full relative h-15 px-2 py-3 bg-sky-50/20 border border-gray-400 shadow-2xs shadow-black/10 flex items-center justify-between" >
             <Sidebar/>
                 <div className="flex items-center justify-center gap-3 " >
-                    <h1 className="text-2xl font-semibold " > {user} </h1>
-                    <div className="w-10 flex items-center justify-center font-extrabold  bg-sky-100/50 h-10 text-xl rounded-full border-2 border-blue-500" >
-                        L
+                    <h1 className="text-2xl font-semibold " > {user.name} </h1>
+                    <div className="w-10 flex items-center justify-center font-extrabold capitalize bg-sky-100/50 h-10 text-xl rounded-full border-2 border-blue-500" >
+                        {user.name.split("")[0]}
                     </div>
                 </div>
             </nav>
--- app/teacher/page.jsx
+++ app/teacher/page.jsx
@@ -3,9 +3,26 @@ import Navbar from "../components/teacher/navbar";
 import { Inter } from "next/font/google";
 import { Clock } from "react-bootstrap-icons";
 import { Pass } from "react-bootstrap-icons";
+import supabase from "../supabase/supabase";
+import { createClient } from "../supabase/server";
+import moment from "moment";
 const inter = Inter({})
 
-export default function Page() {
+export default async function Page() {
+    const supabaseauth = await createClient()
+    const { data: id } = await supabaseauth.auth.getUser()
+    const { data: teacherid } = await supabase.schema("sekolah").from("teacher").select("id,name").eq("email", id.user.id).single()
+    const { data: assignment, } = await supabase.schema("sekolah").from("assignment").select("*").eq("id", teacherid.id)
+    const { data: nextschedule } = await supabase.schema("sekolah").from("schedule").select("*,class_id(class)").eq("teacher", teacherid.id)
+    const scheduletime = nextschedule.map(e => ({timestart: moment(e.time_start, "HH:mm:ssZ").format("HH:mm"),name: e.name,nextclass:e.class_id.class }));
+
+    const nextSchedule = scheduletime
+        .filter(item => moment(item.timestart, "HH:mm").isAfter(moment("10:30", "HH:mm")))
+        .sort((a, b) =>
+            moment(a.time, "HH:mm").diff(moment(b.time, "HH:mm"))
+        )[0];
+        console.log(scheduletime)
+
     return (
         <div className={"flex w-screen flex-col " + inter.className} >
             <Navbar />
@@ -22,16 +39,16 @@ export default function Page() {
                     <div className="w-full h-30 bg-blue-600 rounded-xl justify-between flex flex-col gap-1 px-3 py-1">
                         <div className="flex flex-col w-full capitalize  justify-between" >
                             <h4 className="text-xl font-medium text-gray-300" > kelas berikutnya </h4>
-                            <h4 className="text-md font-thin text-white rounded-full w-max" > Tbg-c </h4>
+                            <h4 className="text-md font-thin text-white rounded-full w-max" > {nextSchedule.nextclass} </h4>
                         </div>
-                        <h1 className="flex items-center gap-1 text-white font-medium uppercase text-xl" > <Clock /> 09:00 AM  </h1>
+                        <h1 className="flex items-center gap-1 text-white font-medium uppercase text-xl" > <Clock /> {nextSchedule.timestart}  </h1>
                     </div>
                     <div className="flex flex-col h-30 w-full bg-gray-300 rounded-2xl" >
                         <div className="text-2xl justify-between p-2 flex w-full items-center font-semibold uppercase text-blue-600" >
                             <h2 className="w-max bg-blue-100 p-2 rounded-xl font-semibold flex items-center justify-center" >
                                 <Pass />
                             </h2>
-                            <h1> 4 </h1>
+                            <h1> {assignment.length} </h1>
                         </div>
                         <h1 className="text-xl px-2 font-semibold text-gray-800 capitalize" > Total tugas yang diberikan </h1>
                     </div>
